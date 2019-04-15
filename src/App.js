84b267e5a56b746b9cd9d7d361312ac9726cbe43
nikilarigela <nikil.arigela@gmail.com>
2019-04-15 15:34:25 +0530

    new version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import logo from "./animated.jpg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <div className="top-content" />
+          <img className="logo" src={logo} alt="Logo" />
+          <div className="content">
+            <p>Hi, I'm Kartik.</p>
+            <p>
+              <span>I'm a Software Engineer,</span>
+              <br />
+              currently working on Mobile <br />
+              <span class="bottom-line">Application Developement </span>
+            </p>
+            <FontAwesomeIcon icon="envelope" color="#ff0" />
+          </div>
         </header>
       </div>
     );

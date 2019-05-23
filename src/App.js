33cc5c25e0d856d677ff2df8fc0d738da8019433
nikilarigela <nikil.arigela@gmail.com>
2019-05-23 11:08:22 +0530

    feat: added social links and changed font

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./animated.jpg";
+import instaLogo from "./instagramIcon.png";
+import twitterLogo from "./twitterIcon.png";
+import githubLogo from "./githubIcon.png";
 
 class App extends Component {
+  openLinkInNewTab = url => {
+    var win = window.open(url, "_blank");
+    win.focus();
+  };
   render() {
     return (
       <div className="App">
@@ -15,8 +22,33 @@ class App extends Component {
               <span>I'm a Software Engineer,</span>
               <br />
               currently working on Mobile <br />
-              <span class="bottom-line">Application Developement </span>
+              <span class="bottom-line">Application Developement. </span>
             </p>
+          </div>
+          <div className="social-links">
+            <img
+              className="social-icon"
+              src={twitterLogo}
+              onClick={() =>
+                this.openLinkInNewTab("https://twitter.com/nikilarigela")
+              }
+            />
+            <img
+              className="social-icon"
+              src={githubLogo}
+              onClick={() =>
+                this.openLinkInNewTab("https://github.com/nikilarigela")
+              }
+            />
+            <img
+              className="social-icon"
+              src={instaLogo}
+              onClick={() =>
+                this.openLinkInNewTab(
+                  "https://www.instagram.com/nikilarigela0110/"
+                )
+              }
+            />
           </div>
         </header>
       </div>
